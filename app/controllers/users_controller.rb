baf22e4bf0d94569611c689a7dffedcfe0e17163
ne_Sachirou <utakata.c4se@gmail.com>
2015-03-07 03:26:09 +0900

    Adjust to Client-Android initial commit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit :password
+    # params.require(:user).permit :password
+    params.permit :password
   end
 end
--- app/views/movies/index.json.jbuilder
+++ app/views/movies/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@movies) do |movie|
-  json.extract! movie, :count, :latitude, :longitude, :uuid
+  json.extract! movie, :count, :latitude, :longitude, :uuid, :created_at, :updated_at
   json.url movie_url(movie.uuid.to_param, format: :json)
 end
